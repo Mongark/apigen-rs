adb507856c27683a628c72e2c15464f5ac41f982
Mongark <mongarkprogrammer@gmail.com>
2023-01-02 14:33:20 -0400

    Added error messages to get_apirc_from

--- src/config/src/loader.rs
+++ src/config/src/loader.rs
@@ -2,9 +2,10 @@ use std::fs::read_to_string;
 use datatypes::api_types::APIRC;
 
 pub fn get_apirc_from(address: &String) -> APIRC {
-    let data: String = read_to_string(&address).unwrap();
-
-    let apirc: APIRC = serde_json::from_str(&data).unwrap();
+    let data: String = read_to_string(&address)
+        .expect("Error reading file");
+    let apirc: APIRC = serde_json::from_str(&data)
+        .expect("Invalid config file");
 
     apirc
 }
